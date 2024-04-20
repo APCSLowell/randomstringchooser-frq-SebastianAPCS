1a7f6cc77681c8695f94a3c0e48116e30dddfef9
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-20 18:06:23 +0000

    implement RandomLetterChooser & RandomStringChooser classes

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,19 +1,14 @@
-public class RandomLetterChooser extends RandomStringChooser
-{
+public class RandomLetterChooser extends RandomStringChooser {
 	
-	public RandomLetterChooser(String str)
-	{ 
-		/* to be implemented in part (b) */
-		
+	public RandomLetterChooser(String str) { 
+		super(getSingleLetters(str));
 	}
 	
 	
-	public static String[] getSingleLetters(String str)
-	{ 
+	public static String[] getSingleLetters(String str) { 
 		String[] letterArray = new String[str.length()];
-		for(int i=0; i<str.length(); i++)
-		{
-			letterArray[i] = ""+str.charAt(i);
+		for (int i = 0; i < str.length(); i++) {
+			letterArray[i] = "" + str.charAt(i);
 		}
 		return letterArray;
 	}
--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,26 @@
 import java.util.*;
-public class RandomStringChooser
-{
-  /* to be implemented in part (a) */
-  
-  //Heads up! 
-  //You will get a very confusing error message until you have working code in part b as well
+public class RandomStringChooser {
+  ArrayList<String> strings;
+
+  public RandomStringChooser(String[] words) {
+    strings = new ArrayList<>();
+
+    for (String word : words) {
+      strings.add(word);
+    }
+  }
+
+  public String getNext() {
+    String word;
+    if (strings.size() != 0) {
+      int randomNum = (int) (Math.random() * strings.size());
+      
+      word = strings.get(randomNum);
+      strings.remove(randomNum);
+    } else {
+      word = "NONE";
+    }
+
+    return word;
+  }
 }
